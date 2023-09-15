527b265ac909d2eb5edba2d702bd8ad11d1bae11
Ernst-D <ernst.dzerevanko@gmail.com>
2023-09-15 15:59:49 +0400

    more codemagic code

--- scripts/load-ios-app.mjs
+++ scripts/load-ios-app.mjs
@@ -1,14 +1,25 @@
 import "zx/globals";
 const { CODEMAGIC_API_KEY, APP_ID } = process.env;
 
-const apps = async () => await $`curl -H "Content-Type: application/json" \
--H "x-auth-token: ${CODEMAGIC_API_KEY}" \
---request GET https://api.codemagic.io/apps`;
+async function apps() {
+    await $`curl -H "Content-Type: application/json" \
+    -H "x-auth-token: ${CODEMAGIC_API_KEY}" \
+    --request GET https://api.codemagic.io/apps`;
+}
 
 const builds = async () => await $`curl -H "Content-Type: application/json" \
 -H "x-auth-token: ${CODEMAGIC_API_KEY}" \
 --request GET https://api.codemagic.io/builds?appId=${APP_ID}`;
 
+// see: https://docs.codemagic.io/rest-api/artifacts/
+const appLink = async (url) => await $`curl -H "x-auth-token: ${CODEMAGIC_API_KEY}" \
+--request GET ${url}`.pipe(process.stdout);
+
 const res = await builds();
 
-console.log(JSON.parse(res.stdout));
+//latest app
+const downloadUrl = JSON.parse(res.stdout)["builds"][0]["artefacts"][0]["url"];
+
+console.log(JSON.parse(res.stdout)["builds"]);
+console.log(downloadUrl);
+
