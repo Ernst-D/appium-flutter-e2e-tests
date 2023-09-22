6b990e37b64d72e78aefe4aa594e21043fc4048f
Ernst-D <ernst.dzerevanko@gmail.com>
2023-09-22 12:31:35 +0400

    edited workflow for test purposes

---
+++ .github/scripts/build_wda_sim.sh
@@ -0,0 +1,6 @@
+xcodebuild build-for-testing \
+  -project ./node_modules/appium-flutter-driver/node_modules/appium-xcuitest-driver/node_modules/appium-webdriveragent/WebDriverAgent.xcodeproj \
+  -derivedDataPath app/wda \
+  -scheme WebDriverAgentRunner \
+  -destination "platform=iOS Simulator,name=$DEVICE_NAME" \
+  CODE_SIGNING_ALLOWED=NO
