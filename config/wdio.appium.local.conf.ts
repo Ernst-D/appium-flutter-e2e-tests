bfb63ae3716811321984125561cc2c794f5d760a
Ernst-D <ernst.dzerevanko@gmail.com>
2023-09-22 13:45:00 +0400

    adjust connection timeout for local config to 4 minutes

--- config/wdio.appium.local.conf.ts
+++ config/wdio.appium.local.conf.ts
@@ -29,6 +29,8 @@ config.services = [
     } ]
 ];
 
+config.connectionRetryTimeout = 240000;
+
 config.before = async function () {
     await driver.switchContext("NATIVE_APP");
     await driver.execute("mobile: terminateApp", { bundleId: "com.apple.mobilesafari" });
