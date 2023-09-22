302b81e568a51d4052dd7e4c68fc0f9b8db0ddc2
Ernst-D <ernst.dzerevanko@gmail.com>
2023-09-22 12:46:46 +0400

    pwd for build script

--- .github/scripts/build_wda_sim.sh
+++ .github/scripts/build_wda_sim.sh
@@ -1,5 +1,5 @@
 xcodebuild build-for-testing \
-  -project ./node_modules/appium-flutter-driver/node_modules/appium-xcuitest-driver/node_modules/appium-webdriveragent/WebDriverAgent.xcodeproj \
+  -project $PWD/node_modules/appium-flutter-driver/node_modules/appium-xcuitest-driver/node_modules/appium-webdriveragent/WebDriverAgent.xcodeproj \
   -derivedDataPath app/wda \
   -scheme WebDriverAgentRunner \
   -destination "platform=iOS Simulator,name=$DEVICE_NAME" \
